cc34b3b03f9d1242063dabc5235941c46de86048
jawadali <jawadali20017@gmail.com>
2023-02-18 15:30:53 +0500

    Booking Form

--- resources/js/components/NewBooking/newBooking.vue
+++ resources/js/components/NewBooking/newBooking.vue
@@ -14,6 +14,7 @@
           <h4 style="margin: 0px 0px;">Booking Details</h4>
         </div>
         <div style="padding: 10px">
+          <el-divider content-position="left"><h3>Booking Details</h3></el-divider>
           <el-row :gutter="15">
             <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
               <el-form-item label="Customer" prop="customer" size="mini">
@@ -96,6 +97,9 @@
                 </el-select>
               </el-form-item>
             </el-col>
+          </el-row>
+          <el-divider content-position="left"><h3>Visa Details</h3></el-divider>
+          <el-row :gutter="15">
             <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
               <el-form-item label="IATA" prop="iata" size="mini">
                 <el-select
@@ -110,6 +114,227 @@
                     :value="item"
                   />
                 </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Visa Company" prop="visaCompany" size="mini">
+                <el-input v-model="newBooking.visaCompany" placeholder="Please Enter Visa Company" size="mini" />
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Visa Status" prop="visaStatus" size="mini">
+                <el-select
+                  v-model="newBooking.visaStatus"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-divider content-position="left"><h3>Hotel Details</h3></el-divider>
+          <el-row :gutter="15">
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Room Type" prop="roomType" size="mini">
+                <el-select
+                  v-model="newBooking.roomType"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Package" prop="package" size="mini">
+                <el-select
+                  v-model="newBooking.package"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Short Booking" prop="shortBooking" size="mini">
+                <el-switch
+                  v-model="newBooking.shortBooking"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="15">
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Hotel 1" prop="hotel1" size="mini">
+                <el-select
+                  v-model="newBooking.hotel1"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Nights 1" prop="nigths1" size="mini">
+                <el-input-number
+                  v-model="newBooking.nights1"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="15">
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Hotel 2" prop="hotel2" size="mini">
+                <el-select
+                  v-model="newBooking.hotel2"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Nights 2" prop="nigths2" size="mini">
+                <el-input-number
+                  v-model="newBooking.nights2"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="15">
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Hotel 3" prop="hotel3" size="mini">
+                <el-select
+                  v-model="newBooking.hotel3"
+                  size="mini"
+                  style="width: 100%"
+                >
+                  <el-option
+                    v-for="item in ['Draft','Final']"
+                    :key="item"
+                    :label="item"
+                    :value="item"
+                  />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Nights 3" prop="nigths3" size="mini">
+                <el-input-number
+                  v-model="newBooking.nights3"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="15">
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Adults" prop="adults" size="mini">
+                <el-input-number
+                  v-model="newBooking.adults"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Childs" prop="childs" size="mini">
+                <el-input-number
+                  v-model="newBooking.childs"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :lg="4" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Infants" prop="infants" size="mini">
+                <el-input-number
+                  v-model="newBooking.infants"
+                  :max="10"
+                  :min="1"
+                  controls-position="right"
+                  style="width: 100%"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-divider content-position="left"><h3>Confirmation Details</h3></el-divider>
+          <el-row :gutter="15">
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Approval Date" prop="approvalDate" size="mini">
+                <el-date-picker
+                  v-model="newBooking.approvalDate"
+                  placeholder="Pick a day"
+                  size="mini"
+                  style="width: 100%"
+                  type="date"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Expected Departure" prop="expectedDeparture" size="mini">
+                <el-date-picker
+                  v-model="newBooking.expectedDeparture"
+                  placeholder="Pick a day"
+                  size="mini"
+                  style="width: 100%"
+                  type="date"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :lg="8" :md="12" :sm="24" class="card-panel-col">
+              <el-form-item label="Confirmed Ticket" prop="confirmedTicket" size="mini">
+                <el-switch
+                  v-model="newBooking.confirmedTicket"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                />
               </el-form-item>
             </el-col>
           </el-row>
